4e2b3c748e35d31b3b91a887e7ca2c42d9723c0f
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2021-03-25 19:41:26 -0300

    add exercise

---
+++ assets/js/aulas/traversing-manipulacao.js
@@ -0,0 +1,21 @@
+// Duplique o menu e adicione ele em copy
+// const menu = document.querySelector('.menu');
+// const novoMenu = menu.cloneNode(true);
+// const copy = document.querySelector('.copy')
+
+// copy.appendChild(novoMenu);
+
+// Selecione o primeiro DT da dl de Faq
+// const faq = document.querySelector('.faq')
+// const primeiroDT = faq.querySelector('dt')
+// console.log(primeiroDT)
+
+// // Selecione o DD referente ao primeiro DT
+// const primeiroDD = primeiroDT.nextElementSibling;
+// console.log(primeiroDD)
+
+// Substitua o conteúdo html de .faq pelo de .animais
+// const animais = document.querySelector('.animais');
+// const faq = document.querySelector('.faq')
+
+// faq.innerHTML = animais.innerHTML;
