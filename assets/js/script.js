db9c5ba665c20c384136030b5c9ed2a993a535ff
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2021-03-31 08:52:52 -0300

    modify tabnav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    const activeClass = 'ativo';
 
-if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add(activeClass);
 
-    function classAtivo(index){
-        tabContent.forEach(item => item.classList.remove('ativo'))
-        tabContent[index].classList.add('ativo');
+        function classAtivo(index){
+            tabContent.forEach(item => item.classList.remove(activeClass))
+            tabContent[index].classList.add(activeClass);
+        }
+
+        tabMenu.forEach( (li, position) => {
+            li.addEventListener('click', () => classAtivo(position))
+        })
     }
-
-    tabMenu.forEach( (li, position) => {
-        li.addEventListener('click', () => classAtivo(position))
-    })
-}
-
+} initTabNav();
 
