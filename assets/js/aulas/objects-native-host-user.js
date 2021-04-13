381693ebaab06549b981bdd7037c1678d504b633
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2021-04-13 19:16:29 -0300

    add exercise

---
+++ assets/js/aulas/objects-native-host-user.js
@@ -0,0 +1,19 @@
+// Liste 5 objetos nativos
+// if(typeof Number !== 'undefined') return console.log('Nativo')
+// if(typeof String !== 'undefined') return console.log('Nativo')
+// if(typeof Function !== 'undefined') return console.log('Nativo')
+// if(typeof Object !== 'undefined') return console.log('Nativo')
+// if(typeof Array !== 'undefined') return console.log('Nativo')
+
+// Liste 5 objetos do browser
+// HTMLCollection;
+// NodeList;
+// Element;
+// Document
+// Window
+if(typeof Window !== 'undefined') return console.log('Nativo')
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+// geralmente os webkit
