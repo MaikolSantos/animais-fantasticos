26b7a8d902692caecb4912496807118dca27f1b1
Maikol Santos <maikol.lourencon@gmail.com>
2022-08-26 20:31:23 -0300

    finish exercise

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const allImgs = document.querySelectorAll('img');
+// console.log(allImgs)
+
+const imgImagem = document.querySelectorAll('[src^="./assets/img/imagem"]')
+// console.log(imgImagem)
+
+const linksInternal = document.querySelectorAll('[href^="#"')
+// console.log(linksInternal)
+
+const sectionAnimaisDescription = document.querySelector('.animais-descricao')
+// console.log(sectionAnimaisDescription)
+
+const firstH2AnimaisDescription = sectionAnimaisDescription.querySelector('h2')
+// console.log(firstH2AnimaisDescription)
+
+const lastP = document.querySelector('p:last-child')
+// console.log(lastP)
